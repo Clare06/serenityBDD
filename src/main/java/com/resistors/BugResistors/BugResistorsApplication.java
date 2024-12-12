8fe4123b1123be152ed2740a3c17d4278c4707c0
Claretes Nishoban <106062267+Clare06@users.noreply.github.com>
2024-12-12 16:06:58 +0530

    init commit

---
+++ src/main/java/com/resistors/BugResistors/BugResistorsApplication.java
@@ -0,0 +1,13 @@
+package com.resistors.BugResistors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugResistorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugResistorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resistors/BugResistors/BugResistorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resistors.BugResistors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugResistorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
