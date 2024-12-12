9fbcebefebbe6be67dfd2a420c87613cfcd29d5e
Claretes Nishoban <106062267+Clare06@users.noreply.github.com>
2024-12-12 16:21:05 +0530

    fix pack name

--- src/main/java/com/resistors/BugResistorsApplication.java
+++ src/main/java/com/resistors/BugResistorsApplication.java
@@ -1,4 +1,4 @@
-package com.resistors.BugResistors;
+package com.resistors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
